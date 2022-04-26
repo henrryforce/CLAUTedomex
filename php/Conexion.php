df941b8039cd582ada47e23f55a6a9f68eec1df5
gerardoSV95 <gerardo_s_v@outlook.com>
2022-04-26 14:17:44 -0500

    Clase conexion v2

--- php/Conexion.php
+++ php/Conexion.php
@@ -9,9 +9,7 @@
       private $error;
       private $stmt;
 
-
-      public function __construct(){
-        
+      public function __construct(){        
         $dsn = 'mysql:host='.$this->host . ';dbname='. $this->dbname;
         $options = array(
             PDO::ATTR_PERSISTENT    =>true,
@@ -22,6 +20,40 @@
         }catch(PDOEception $e){
           $this->error = $e-> getMessage();
         }
+      }
+
+      public function query($query){
+        $this->stmt = $this->dbh->prepare($query);
+      }
+
+      public function bind($param, $value, $type = null){
+        if(is_null($type)){
+          switch(true){
+            case is_int($value):
+                $type = PDO::PARAM_INT;
+                break;
+            case is_bool($value):
+                $type = PDO::PARAM_BOOL;
+                break;
+            case is_null($value):
+                $type = PDO::PARAM_NULL;
+                break;
+                default: 
+                $type = PDO::PARAM_STR;
+          }
+        }
+        $this->stmt->bindValue($param,$value,$type);
+      }
+
+      public function execute(){
+        return $this->stmt->execute();
+      }
+      public function lastInsertId(){
+        $this->dbh->lastInsertId();
+      }
+      public function resultSet(){
+        $this->execute();
+        return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
       }
     }
 ?>
