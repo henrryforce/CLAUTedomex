3668afb69ce027738846e62238285271764dd435
henrryforce <luis15ago98@gmail.com>
2022-04-27 11:58:33 -0500

    Comentarios en clase conexion

--- php/Conexion.php
+++ php/Conexion.php
@@ -1,5 +1,6 @@
 <?php
-    class Conexion{      
+    class Conexion{
+      /**definimos variabbles de conexion */      
       private $host = "localhost";
       private $user = "root";
       private $pass = "";
@@ -8,7 +9,9 @@
       private $dbh;
       private $error;
       private $stmt;
-
+/**
+ * constructoe de la clase conexion
+ */
       public function __construct(){        
         $dsn = 'mysql:host='.$this->host . ';dbname='. $this->dbname;
         $options = array(
@@ -21,7 +24,9 @@
           $this->error = $e-> getMessage();
         }
       }
-
+/**
+ * metodo para preparar qwerys 
+ */
       public function query($query){
         $this->stmt = $this->dbh->prepare($query);
       }
@@ -44,13 +49,16 @@
         }
         $this->stmt->bindValue($param,$value,$type);
       }
-
+/**
+ * Metodo interno para ejecutar el query
+ */
       public function execute(){
         return $this->stmt->execute();
       }
       public function lastInsertId(){
         $this->dbh->lastInsertId();
       }
+      /**Metodo para mandar llamar el resultado de la ejecucion del query */
       public function resultSet(){
         $this->execute();
         return $this->stmt->fetchAll(PDO::FETCH_ASSOC);
