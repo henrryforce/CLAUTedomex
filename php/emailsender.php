b2a458502579b22c60c48e36a5d14813e915d26f
henrryforce <luis15ago98@gmail.com>
2022-05-11 14:08:36 -0500

    Contacto envia correo al interesado y al admin interno, mail banner de contacto internoy mail banner de contacto al usaurio interesado

--- php/emailsender.php
+++ php/emailsender.php
@@ -133,6 +133,52 @@ class enviarCorreo{
         //$mail->AltBody = 'Este es el cuerpo en texto sin formato para clientes de correo que no son HTML';
 
         $mail->send();    //Enviar correo eletrónico
+        $this -> enviarCorreoF();
+        return 'El mensaje ha sido enviado';
+    } catch (Exception $e) {
+        return "No se pudo enviar el mensaje. Error de correo: {$mail->ErrorInfo}";
+    }
+    
+    }
+    function enviarCorreoF(){
+        $mail = new PHPMailer(true);
+    try {
+        //Configuración del servidor
+        $mail->SMTPDebug = SMTP::DEBUG_OFF;             //Habilitar los mensajes de depuración
+        $mail->isSMTP();                                   //Enviar usando SMTP
+        $mail->Host       = 'smtp.gmail.com';            //Configurar el servidor SMTP
+        $mail->SMTPAuth   = true;                          //Habilitar autenticación SMTP
+        $mail->Username   = 'allstarblaster98@gmail.com';            //Nombre de usuario SMTP
+        $mail->Password   = 'zorabel98';                      //Contraseña SMTP
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;   //Habilitar el cifrado TLS
+        $mail->Port       = 587;                           //Puerto TCP al que conectarse; use 587 si configuró `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+        //Emisor
+        $mail->setFrom('allstarblaster98@gmail.com', 'Contacto BAM 24/7');
+
+        //Destinatarios
+        $mail->addAddress($this->to);     //Añadir un destinatario, el nombre es opcional
+
+        //Destinatarios opcionales
+        $mail->addReplyTo('admin@example.com', 'Contacto');  //Responder a
+        //$mail->addCC('cc@example.com');                        //Copia pública
+        //$mail->addBCC('bcc@example.com');                      //Copia oculta
+
+        //Archivos adjuntos
+        //$mail->addAttachment('img1.jpeg', 'Comunicado');         //Agregar archivos adjuntos, nombre opcional
+        
+
+        //Nombre opcional
+        $mail->isHTML(true);                         //Establecer el formato de correo electrónico en HTMl
+        $mail->CharSet = 'UTF-8';
+        $mail->Subject = "Recibimos tu solicitud";
+        $mail->AddEmbeddedImage("logo-main.png", "my-attach", "logo-main.png");        //path, ID a buscar cid:el ID, nombre archivo  
+        $html = file_get_contents('ContactoU.html');
+        
+        $mail->Body    = $html;
+        //$mail->AltBody = 'Este es el cuerpo en texto sin formato para clientes de correo que no son HTML';
+
+        $mail->send();    //Enviar correo eletrónico
         return 'El mensaje ha sido enviado';
     } catch (Exception $e) {
         return "No se pudo enviar el mensaje. Error de correo: {$mail->ErrorInfo}";
