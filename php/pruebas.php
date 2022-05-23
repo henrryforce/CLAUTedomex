b016ff727232c35983c75b6b4f5ff6f549cdf0d2
henrryforce <luis15ago98@gmail.com>
2022-05-22 22:43:41 -0500

    Desplegando datos de la BD prellenando los form con ella para su edicion

--- php/pruebas.php
+++ php/pruebas.php
@@ -6,10 +6,10 @@ include 'Conexion.php';
 $obj = new Conexion();
 //session_destroy();
 
-$obj -> query("SELECT  `paisesExporta` as pais FROM `exportrequeridas` WHERE `idcomprador` = $id");
+$obj -> query("SELECT  `Logo` FROM `archivos` WHERE `ID_archivo` = 12");
 $res= $obj -> resultSet();
-if(isset($_GET['expo']) && $_GET['expo'] == 501 ){
-    echo json_encode($res);
-}
+print_r($res);
+echo ('<br>');
+echo ($res[0]['Logo'] =='' ) ? 'php/archivosUpload/logos/default.png': $res[0]['Logo'];
 
 ?>
